e858bb2aef2cd993fd8aa3408bf4c01aeae406ac
kavishamodi97 <kavishamodi971@gmail.com>
2021-05-03 22:42:07 +0530

    Commit With Implemented Signup Modal and Styling for Signup Modal

---
+++ scripts/common.js
@@ -0,0 +1,9 @@
+/* Scripts for Header Section */
+
+function showSignUpModal() {
+document.getElementById('signup-modal').style.display = 'flex';
+}
+
+function DismissSignUpModal(){
+    document.getElementById('signup-modal').style.display='none';
+}

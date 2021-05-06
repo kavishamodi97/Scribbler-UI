3939bb563ebdad46615b9d74025973c8ec3a2045
kavishamodi97 <kavishamodi971@gmail.com>
2021-05-06 18:14:13 +0530

    Commit With Fix Delete Modal UI and Implemented Delete Card Functionality

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,36 +1,22 @@
 /* Script For PostList Page*/
 
-var modal;
-function deleteModal(elementId) {
-    modal = document.getElementById('delete-modal')
-    modal.style.display = 'block';
-    document.getElementsByClassName('yes-btn')[0].id = elementId;
+var cardModalId;
+
+function deleteCardModal(id) {
+    cardModalId = id;
+    return id;
 }
 
-function deletePost(postid) {
-    var finalid = "post" + postid;
-    document.getElementById(`${finalid}`).style.display = 'none';
-    modal.style.display = 'none';
-}
-
-function closeDeleteModal() {
-    modal.style.display = 'none';
-}
-
-function openPost(id) {
-    var title = document.getElementById(`title-${id}`).childNodes;
-    title = title[1].innerHTML;
-    var description = document.getElementById(`desc-${id}`).innerHTML;
-    var author = document.getElementById(`post-author-${id}`).childNodes;
-    author = author[1].innerHTML;
-    localStorage.setItem('title', title);
-    localStorage.setItem('description', description);
-    localStorage.setItem('author', author);
-    window.location.href = 'post.html';
-}
-
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+function deleteCard(postid) {
+    for (var cardIndex = 0; cardIndex <= 5; cardIndex++) {
+        if (cardIndex == deleteCardModal(cardModalId)) {
+            cardIndex = "post" + cardIndex;
+            var delCardModal = document.getElementById(cardIndex).style.display = 'none';
+        }
     }
 }
+
+function navigateToPostPage() {
+    window.location.href = "post.html";
+}
+

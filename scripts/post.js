385a473414741429d064007553e300dc852cc964
kavishamodi97 <kavishamodi971@gmail.com>
2021-05-07 20:18:56 +0530

    Commit With Created Comment Section and Styling Comment Section

--- scripts/post.js
+++ scripts/post.js
@@ -3,7 +3,8 @@
 window.onload = function () {
     var sessionObj = sessionStorage.getItem('postInfo');
     var parseObj = JSON.parse(sessionObj);
-    document.getElementById('postAuthor').innerHTML = parseObj["authorName"] + `<button type="button" class="edit-btn" id="editPostBtn">Edit <i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>`;
+    var editButton =`<button type="button" class="edit-btn" id="editPostBtn">Edit <i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>`;
+    document.getElementById('postAuthor').innerHTML = parseObj["authorName"] + editButton ;
     document.getElementById('postTitle').innerHTML = parseObj["titleName"];
     document.getElementById('postDescription').innerHTML = parseObj["description"];
 }

40058983b3ae1f026d8c7452efd2615df4e5de36
kavishamodi97 <kavishamodi971@gmail.com>
2021-05-08 21:44:33 +0530

    Commit With Code Formatting

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 /* Script For Home Page*/
 
-/* Click On All Posts Button Navigate To PostList Page */ 
+/* Click On All Posts Button Navigate To PostList Page */
 function navigateToPostListPage() {
     window.location.href = "html/postslist.html";
 }

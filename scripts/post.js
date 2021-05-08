34afd7091d664992278ad543b0b0bde3fae0d9db
kavishamodi97 <kavishamodi971@gmail.com>
2021-05-08 17:34:40 +0530

    Commit With Implemented Like Functionality

--- scripts/post.js
+++ scripts/post.js
@@ -1,10 +1,26 @@
 /* Script For PostList Page*/
 
+
 window.onload = function () {
     var sessionObj = sessionStorage.getItem('postInfo');
     var parseObj = JSON.parse(sessionObj);
-    var editButton =`<button type="button" class="edit-btn" id="editPostBtn">Edit <i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>`;
-    document.getElementById('postAuthor').innerHTML = parseObj["authorName"] + editButton ;
+    var editButton = `<button type="button" class="edit-btn" id="editPostBtn">Edit <i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>`;
+    document.getElementById('postAuthor').innerHTML = parseObj["authorName"] + editButton;
     document.getElementById('postTitle').innerHTML = parseObj["titleName"];
     document.getElementById('postDescription').innerHTML = parseObj["description"];
+}
+
+var likeCount = 0;
+function likePost() {
+    likeCount++;
+    if (likeCount === 1) {
+        document.getElementById('like-container').innerHTML =
+            `<button type="button" id="likeBtn" class="like-btn" onclick="likePost()"><i class="fa fa-thumbs-up"></i> Liked</button>` +
+            `<p id="like-text">${likeCount} person likes this!</p>`;
+    } else if (likeCount > 1) {
+        document.getElementById('like-container').innerHTML =
+            `<button type="button" id="likeBtn" class="like-btn" onclick="likePost()"><i class="fa fa-thumbs-up"></i>  Liked</button>` +
+            `<p id="like-text">${likeCount} people like this!</p>`;
+    }
+
 }
